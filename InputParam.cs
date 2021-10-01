e541ff9587158fa02ef90bb14925601a8dda06e6
Aleksey Burkov <burkov.aleksey.main@gmail.com>
2021-10-01 18:44:07 +0400

    Initial class for processing input parameters

---
+++ InputParam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenScv
+{
+    class InputParam
+    {
+        int columns = 4;
+        int rows = 20;
+        int length = 15;
+        string encoding = "UTF-8";
+        string outFile = "out.csv";
+        public int Columns => columns;
+        public int Rows => rows;
+        public int Length => length;
+        public string Encoding => encoding;
+        public string OutFile => outFile;
+
+        
+    }
+}
